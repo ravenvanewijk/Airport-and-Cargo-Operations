1523b0fcc315b2da40f319c7e359d037f144dc31
ravenvanewijk <ravenvanewijk1@gmail.com>
2023-03-23 12:02:13 +0100

    pickles geladen

--- Assignment.py
+++ Assignment.py
@@ -1,1 +1,7 @@
-print('t')
+import pickle5 as pickle
+
+with open('G11\G1\B.pickle', 'rb') as handle:
+    B = pickle.load(handle)
+
+with open('G11\G1\R.pickle', 'rb') as handle:
+    R = pickle.load(handle)
