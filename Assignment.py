bb26d6b454466cf0a490d1ceaf50b537404f09f4
ravenvanewijk <ravenvanewijk1@gmail.com>
2023-03-23 12:54:58 +0100

    objective erin

--- Assignment.py
+++ Assignment.py
@@ -1,12 +1,14 @@
 import pickle5 as pickle
 import gurobipy as gb
 
+# Load pickle file, B = bins, R = items
 with open('G11\G1\B.pickle', 'rb') as handle:
     B = pickle.load(handle)
 
 with open('G11\G1\R.pickle', 'rb') as handle:
     R = pickle.load(handle)
 
+# Define sets
 bin_set = B.keys()
 item_set = R.keys()
 a_set = [1,2]
@@ -34,8 +36,14 @@ for i in item_set:
         r[i,a] = m.addVar(vtype=gb.GRB.BINARY)
     for j in bin_set:
         p[i,j] = m.addVar(vtype=gb.GRB.BINARY)
-        u[j] = m.addVar(vtype=gb.GRB.BINARY)
+        u[j] = m.addVar(obj=B[j][1][3],vtype=gb.GRB.BINARY)
         for k in item_set:
             x[i,k,j] = m.addVar(vtype=gb.GRB.BINARY)
             z[i, k, j] = m.addVar(vtype=gb.GRB.BINARY)
 
+print('ADDED DECISION VARIABLES')
+
+m.update()
+m.setObjective(m.getObjective(), gb.GRB.MINIMIZE)
+
+print('OBJECTIVE SET')
